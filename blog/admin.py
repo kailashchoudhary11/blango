f57a255fd5fc3966b02b3b8b31d9fbfad0cf0693
Gaming Kai <kaijat108@gmail.com>
2022-07-31 03:47:40 +0000

    Finish Introduction and Django Admin

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from blog.models import Post, Tag
+
+# Register your models here.
+
+admin.site.register(Tag)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+  prepopulated_fields = {"slug": ("title",)}
+ # exclude = ["slug"] # Prevents us from editing these fields in the admin site
+  list_display = ["slug", "published_at", ]
+
