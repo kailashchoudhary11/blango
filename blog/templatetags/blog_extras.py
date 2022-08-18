94fd599b29643ea8c75d7c6d2ce2f610f9a37a91
Kailash Choudhary <kaijat108@gmail.com>
2022-08-18 09:45:01 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,5 +1,7 @@
+from blog.models import Post
 from django import template
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 from django.utils.html import escape, format_html
 from django.utils.safestring import mark_safe
 
@@ -7,9 +9,12 @@ register = template.Library()
 user_model = get_user_model()
 
 @register.filter  
-def author_details(author):
+def author_details(author, user):
   if not isinstance(author, user_model):
     return ""
+  
+  if author == user:
+    return format_html("<strong>me</strong>")
   
   firstname = author.first_name
   lastname= author.last_name
@@ -28,3 +33,58 @@ def author_details(author):
       suffix = ""
 
   return format_html("{}{}{}", prefix, name, suffix)
+
+@register.simple_tag(takes_context=True)  
+def author_details_tag(context):
+  request = context["request"]
+  user = request.user
+  post =  context["post"]
+  author = post.author
+  if not isinstance(author, user_model):
+    return ""
+  
+  if author == user:
+    return format_html("<strong>me</strong>")
+  
+  firstname = author.first_name
+  lastname= author.last_name
+  email = author.email
+
+  if firstname and lastname:
+      name = f"{firstname} {lastname}"
+  else:
+      name = f"{author.username}"
+  
+  if email:     
+      prefix = format_html('<a href="mailto:{}">', email)
+      suffix = format_html("</a>")
+  else:
+      prefix = ""
+      suffix = ""
+
+  return format_html("{}{}{}", prefix, name, suffix)
+
+
+@register.simple_tag
+def row(extra_classes=""):
+  return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+  return format_html('</div>')
+
+@register.simple_tag
+def col(extra_classes=""):
+  return format_html('<div class="col {}">', extra_classes)
+
+@register.simple_tag
+def endcol():
+  return format_html('</div>')
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+  posts = Post.objects.exclude(pk=post.pk)[:5]
+  return {"posts": posts, "title": "Recent Posts"}
+
+
+  
--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
-from blog.views import index
+from blog.views import index, post_detail
 from django.urls import path
 
 app_name = "blog"
 
 urlpatterns = [
   path("", index, name="index"),
+  path("post/<slug:slug>", post_detail, name="blog-post-detail"),
 ]
